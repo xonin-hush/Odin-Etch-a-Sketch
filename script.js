dfc7fb9c3e24cf7bcbc0ab4006a528e2a520075c
xonin-hush <xonindev@gmail.com>
2023-07-14 23:32:43 +0100

    Align grid-items , Hover to change item color

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const container = document.querySelector('#container');
 container.classList.add('grid-container')
 console.log(container)
-for (i = 0; i <(16*16); i++) {
-    
-    const gridItem=document.createElement('div')
-    gridItem.textContent='hi'
+let itemNum = 16
+container.style.gridTemplateColumns = `repeat(${itemNum},1fr)`;
+container.style.gridTemplateColumns = `repeat(${itemNum},1fr)`;
+for (let i = 0; i < (itemNum * itemNum); i++) {
+    const gridItem = document.createElement('div')
     gridItem.classList.add('grid-item')
-    console.log(gridItem)
     container.appendChild(gridItem)
+    gridItem.addEventListener('mouseover', () => {
+        gridItem.classList.remove('grid-item')
+        gridItem.classList.add('color');
+        console.log(gridItem)
+    });
 }
-
 
