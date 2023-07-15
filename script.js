246cb42d02145fd2b4ee93db088aca6e79dbeed8
xonin-hush <xonindev@gmail.com>
2023-07-15 23:22:58 +0100

    Fix items sizes to fit in container

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 const container = document.querySelector('#container');
+const cellSize = 40 / itemNum + "rem"
 container.classList.add('grid-container')
-console.log(container)
-let itemNum = 16
-const cellWidth=40/itemNum +"rem"
-const cellHight=40/itemNum +"rem"
-console.log(cellHight)
-console.log(cellWidth)
+let itemNum = 50
 container.style.gridTemplateColumns = `repeat(${itemNum},1fr)`;
 container.style.gridTemplateRows = `repeat(${itemNum},1fr)`;
 for (let i = 0; i < (itemNum * itemNum); i++) {
     const gridItem = document.createElement('div')
     gridItem.classList.add('grid-item')
-    gridItem.style.width=cellWidth
-    gridItem.style.height=cellHight
+    gridItem.style.width = cellSize
+    gridItem.style.height = cellSize
     container.appendChild(gridItem)
     gridItem.addEventListener('mouseover', () => {
         gridItem.classList.remove('grid-item')
         gridItem.classList.add('color');
-        console.log(gridItem)
     });
 }
 
