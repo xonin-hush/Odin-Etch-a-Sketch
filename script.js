d183c0ca3a8ea044e13d93ea08c8dc530ff6f5b2
xonin-hush <xonindev@gmail.com>
2023-07-17 14:37:10 +0100

    Add functions , Add reset button

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
 const container = document.querySelector('#container');
 container.classList.add('grid-container')
 const sizeButton = document.querySelector('#change-size')
-
 let itemNum = getResolution()
-container.style.gridTemplateColumns = `repeat(${itemNum},1fr)`;
-container.style.gridTemplateRows = `repeat(${itemNum},1fr)`;
-const cellSize = 40 / itemNum + "rem"
-for (let i = 0; i < (itemNum * itemNum); i++) {
-    const gridItem = document.createElement('div')
-    gridItem.classList.add('grid-item')
-    gridItem.style.width = cellSize
-    gridItem.style.height = cellSize
-    container.appendChild(gridItem)
-    gridItem.addEventListener('mouseover', () => {
-        gridItem.classList.remove('grid-item')
-        gridItem.classList.add('color');
-        gridItem.style.backgroundColor=randomRGB
+createGrid(itemNum)
+
+function createGrid(itemNum) {
+    container.style.gridTemplateColumns = `repeat(${itemNum},1fr)`;
+    container.style.gridTemplateRows = `repeat(${itemNum},1fr)`;
+    const cellSize = 40 / itemNum + "rem"
+    for (let i = 0; i < (itemNum * itemNum); i++) {
+        const gridItem = document.createElement('div')
+        gridItem.setAttribute("id", "grid-item")
+        gridItem.classList.add('color-dark-blue')
+        gridItem.style.width = cellSize
+        gridItem.style.height = cellSize
         container.appendChild(gridItem)
-        console.log(randomRGB())
-    });
+        gridItem.addEventListener('mouseover', () => {
+            gridItem.classList.remove('color-dark-blue')
+            gridItem.classList.add('color-sky-blue');
+
+        });
+    }
 }
+
 function getResolution() {
     size = prompt("Please enter number of squares on each side", 16)
-    if(size>100){
-        alert("the max is 100")
-    reset()}
-    else    return size;
+        if ((size <= 100) && (size > 0))
+            return size;
+        else {
+            alert("enter a number between 1 and 100")
+            history.go()
+        }
 }
-function reset() {
-    history.go()
+
+function resetColor() {
+    gridItems = document.querySelectorAll('#grid-item')
+    gridItems.forEach((gridItem) => gridItem.classList.add('color-dark-blue'))
 }
+
+
